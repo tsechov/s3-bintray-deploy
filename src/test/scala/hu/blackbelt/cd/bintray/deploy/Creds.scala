db17a299970688cb8810aeb35f26eab035f4520a
Akos Mocsanyi <tsechov@gmail.com>
2016-04-23 23:44:13 +0200

    gitlab

--- src/test/scala/hu/blackbelt/cd/bintray/deploy/Creds.scala
+++ src/test/scala/hu/blackbelt/cd/bintray/deploy/Creds.scala
@@ -1,16 +1,31 @@
 package hu.blackbelt.cd.bintray.deploy
 
-import java.io.FileInputStream
+import java.io.{File, FileInputStream}
 import java.util.Properties
 
 import org.scalatest.{BeforeAndAfter, Suite}
 
 trait Creds extends BeforeAndAfter {
   this: Suite =>
+
+  def fillFromEnv(prop: Properties) = {
+    def put(key: String) = sys.env.get(key).map(prop.put(key, _))
+    put(Access.aws_accessKeyId)
+    put(Access.aws_secretKey)
+    put(Access.bintray_organization)
+    put(Access.bintray_user)
+    put(Access.bintray_apikey)
+  }
+
   before {
     import scala.collection.JavaConverters._
     val prop = new Properties()
-    prop.load(new FileInputStream("env.properties"))
+    val propsFile = new File("env.properties")
+    if (propsFile.exists()) {
+      prop.load(new FileInputStream(propsFile))
+    } else {
+      fillFromEnv(prop)
+    }
     prop.entrySet().asScala.foreach {
       (entry) => {
         sys.props += ((entry.getKey.asInstanceOf[String], entry.getValue.asInstanceOf[String]))
